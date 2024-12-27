f6afc2e5ce03932c6655689a923d136a55684bac
Daniel G.da Silva <danielaguia2002@gmail.com>
2024-12-27 15:50:22 -0300

    Add human choice function
    the function will store three valid choices for later use

--- script/index.js
+++ script/index.js
@@ -1,3 +1,30 @@
 function getComputerChoice() {
-  return Math.floor(Math.random() * 3);
+  let computerChoice = Math.floor(Math.random() * 3);
+
+  switch (computerChoice) {
+    case 0:
+      computerChoice = "rock";
+      break;
+
+    case 1:
+      computerChoice = "paper";
+      break;
+
+    case 2:
+      computerChoice = "scissors";
+      break;
+
+    default:
+      break;
+  }
+
+  return computerChoice;
+}
+
+function getHumanChoice() {
+  let humanChoice = prompt("rock, paper or scissors?").toLowerCase();
+
+  if (humanChoice != "rock" && "paper" && "scissors") {
+    alert("Please, enter one of the valid choices");
+  }
 }
